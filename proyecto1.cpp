bcf614821f6d91d6436345b2262d2503da6d4a0d
gamaerry <gamaerry@gmail.com>
2025-09-14 13:17:33 -0600

    agregado el gitignore y renamed crear_plataforma

--- proyecto1.cpp
+++ proyecto1.cpp
@@ -12,19 +12,19 @@ const int ANCHO_TOTAL = ANCHO_PLATAFORMAS * 2 + ANCHO_PUENTE; //(3+10+3) = 16
 
 // auxiliares para la construccion del puente
 int inicio_del_puente;
-bool va_vertical;
 
-// char32_t puente_o_vacio(int i, int j) {
-//   int primera = ANCHO_PLATAFORMAS;
-//   int ultima = ANCHO_TOTAL - ANCHO_PLATAFORMAS - 1;
-//   if (j == primera || (j-1) == primera) {
-//     return (i == inicio_del_puente ? '_' :' ');
-//   } else return ' ';
-// }
+char32_t punta_del_puente(int i, int j) {
+  int primera = ANCHO_PLATAFORMAS;
+  int ultima = ANCHO_TOTAL - ANCHO_PLATAFORMAS - 1;
+  if (j == primera || (j - 1) == primera) {
+    return (i == inicio_del_puente ? '_' : ' ');
+  } else
+    return ' ';
+}
 
-char32_t **crear_mapa() {
-  srand(time(NULL));  // semilla distinta cada vez
-  inicio_del_puente = rand()%LARGO_TOTAL;
+char32_t **crear_plataforma() {
+  srand(time(NULL)); // semilla distinta cada vez
+  inicio_del_puente = rand() % LARGO_TOTAL;
   // cout << random();
   char32_t **mapa = (char32_t **)malloc(LARGO_TOTAL * sizeof(char32_t *));
   for (int i = 0; i < LARGO_TOTAL; i++)
@@ -35,7 +35,7 @@ char32_t **crear_mapa() {
       if (j % ANCHO_TOTAL < ANCHO_PLATAFORMAS)
         mapa[i][j] = '_';
       else if (j % ANCHO_TOTAL < (ANCHO_PLATAFORMAS + ANCHO_PUENTE))
-        mapa[i][j] = ' ';
+        mapa[i][j] = punta_del_puente(i, j);
       else
         mapa[i][j] = '_';
     }
@@ -59,7 +59,7 @@ void imprimir_mapa(char32_t **mapa) {
 
 int main() {
   char32_t jugador = U'🯅';
-  char32_t **mapa = crear_mapa();
+  char32_t **mapa = crear_plataforma();
   mapa[2][1] = jugador;
   imprimir_mapa(mapa);
   return 0;
