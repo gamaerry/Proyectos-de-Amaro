61cfa9b3e57d6afbeb0cb047042b68528e1c4ba9
gamaerry <gamaerry@gmail.com>
2025-10-27 12:28:42 -0600

    indicada la plataforma de ejecución

--- U2/java/app/src/main/java/AppEnlace.java
+++ U2/java/app/src/main/java/AppEnlace.java
@@ -2,14 +2,17 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 interface MiLibreria extends Library {
-    MiLibreria INSTANCE = Native.load("practica7", MiLibreria.class);
-    void saludo();
-    void saludoConNombre(String nombre);
+  MiLibreria INSTANCE = Native.load("practica7", MiLibreria.class);
+
+  void saludo();
+
+  void saludoConNombre(String nombre);
 }
 
 public class AppEnlace {
-    public static void main(String[] args) {
-        MiLibreria.INSTANCE.saludo();
-        MiLibreria.INSTANCE.saludoConNombre("Luis");
-    }
+  public static void main(String[] args) {
+    System.out.println("Plataforma: " + System.getProperty("os.name") + " - " + System.getProperty("os.arch"));
+    MiLibreria.INSTANCE.saludo();
+    MiLibreria.INSTANCE.saludoConNombre(" Luis");
+  }
 }
