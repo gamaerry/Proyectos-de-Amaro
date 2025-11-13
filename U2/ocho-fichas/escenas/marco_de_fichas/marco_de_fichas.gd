5d91ee132f5e0e18ed6ce00feafa0c6f6eeb159e
gamaerry <gamaerry@gmail.com>
2025-11-13 00:42:45 -0600

    instanciado marco y nivel con cámara

--- U2/ocho-fichas/escenas/marco_de_fichas/marco_de_fichas.gd
+++ U2/ocho-fichas/escenas/marco_de_fichas/marco_de_fichas.gd
@@ -1,20 +1,25 @@
-class_name MarcoDeFichas extends Node2D
+class_name MarcoDeFichas extends Sprite2D
 
 @export var dimension: int = 3 #2, 3, 4 o 5
 var fichas_en_orden_actual: Array[Ficha] #orden actual, 
 
 func _ready() -> void:
 	fichas_en_orden_actual.assign(get_children())
-	fichas_en_orden_actual.shuffle()
+	#fichas_en_orden_actual.shuffle()
 	fichas_en_orden_actual.append(null)
 
 func movimiento_posible(ficha: Ficha) -> int: # 0, 1, 2, 3 o -1
 	var indice: int = fichas_en_orden_actual.find(ficha)
 	var indice_nulo: int = fichas_en_orden_actual.find(null)
 	var menos_dimension: int = -dimension # godot no permite expresiones en la estructura match
+	var movimiento: int = -1
+	print(fichas_en_orden_actual)
 	match indice - indice_nulo:
-		-1: return 0
-		1: return 2
-		dimension: return 3
-		menos_dimension: return 1
-		_: return -1
+		-1: movimiento = 0
+		1: movimiento = 2
+		dimension: movimiento = 3
+		menos_dimension: movimiento = 1
+		_: return movimiento
+	fichas_en_orden_actual[indice] = null
+	fichas_en_orden_actual[indice_nulo] = ficha
+	return movimiento
