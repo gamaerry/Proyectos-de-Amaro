ca02b7487649f54d0f9079d3c63b4577a1e735d0
gamaerry <gamaerry@gmail.com>
2025-11-17 18:31:52 -0600

    consejos implementados con éxito

--- U2/ocho-fichas/escenas/escena_principal/contenedor.gd
+++ U2/ocho-fichas/escenas/escena_principal/contenedor.gd
@@ -4,35 +4,49 @@ const CONSEJOS: Array[String] = [
 	"En la primera casilla siempre irá número más alto o el uno, ningún otro será digno.",
 	"Como es arriba es abajo.",
 	"Come frutas y verduras.",
-	"Piensa a la inversa, pero dentro de la caja.",
+	"¿Cuantas fichas debería tener el tablero de 1x1?",
+	"Piensa fuera de la caja... pero dentro del tablero...",
 	"Si el Diablo existe, Dios existe.",
 	"Vive el presente.",
 	"Practica la gratitud.",
+	"La solución trivial siempre se resuelve de día.",
 	"Siempre la mitad de los logros se obtienen de día y la mitad de noche."
 ]
 
-#@export var consejos: bool = true
+var mostrar_consejos: bool = true
+var bolsa: Array[String] = []  # Shuffle bag
+@export var consejos: bool = true
 @onready var label: Label = $ColorRect/Consejos
 @onready var color: ColorRect = $ColorRect
 
 func _ready() -> void:
+	_rellenar_bolsa()
 	mostrar_consejo_random()
+
+func _rellenar_bolsa() -> void:
+	bolsa = CONSEJOS.duplicate()
+	bolsa.shuffle()
 
 func _espera(segundos: int) -> void:
 	await get_tree().create_timer(segundos).timeout
 
 func mostrar_consejo_random() -> void:
-	#_espera(14)
-	await show_tip()
-	mostrar_consejo_random() # Recursividad infinita
+	if !mostrar_consejos:
+		return
+	elif bolsa.is_empty():
+		_rellenar_bolsa()
+	else:
+		await _espera(14)
+		await show_tip()
+		mostrar_consejo_random() # Recursividad infinita
 
 func show_tip() -> void:
-	label.text = "Consejo: " + CONSEJOS[0]
+	label.text = "Consejo: " + bolsa.pop_front()
 	var tween: Tween = create_tween()
 	tween.tween_property(label, "modulate:a", 1.0, 1.0)
 	tween.parallel().tween_property(color, "modulate:a", 1.0, 1.0)
 	self.visible = true
-	_espera(7)
+	await _espera(7)
 	tween = create_tween()
 	tween.tween_property(label, "modulate:a", 0.0, 1.5)
 	tween.parallel().tween_property(color, "modulate:a", 0.0, 1.5)
