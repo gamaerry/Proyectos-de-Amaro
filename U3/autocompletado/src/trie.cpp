746027a7be8eafe1e8b2c7899b611a450fc27dab
gamaerry <gamaerry@gmail.com>
2025-12-06 10:08:00 -0600

    topk implementado

--- U3/autocompletado/src/trie.cpp
+++ U3/autocompletado/src/trie.cpp
@@ -39,10 +39,8 @@ void Trie::destruir(NodoTrie *nodo) {
   delete nodo;
 }
 
-
-// métodos poco eficientes:
-
-void Trie::recolectar(NodoTrie *nodo, const std::string &prefijo, std::vector<std::pair<std::string, int>> &resultados) const { // TODO: recolectar solo topk con una priority_queue
+// Métodos poco eficientes:
+void Trie::recolectar(NodoTrie *nodo, const std::string &prefijo, std::vector<std::pair<std::string, int>> &resultados) const {
   if (!nodo)
     return;
   if (nodo->fin_de_palabra)
@@ -71,3 +69,49 @@ std::vector<std::pair<std::string, int>> Trie::obtener_sugerencias(const std::st
   return resultados;
 }
 
+// Métodos más optimizado (hacen lo mismo que recolectar() y obtener_sugerencias() pero delimitado a un k, ie. más eficientes):
+std::vector<std::pair<int, std::string>> Trie::obtener_topk(const std::string &prefijo, int k) const {
+  NodoTrie *nodo = buscar_prefijo(prefijo);
+  if (!nodo)
+    return {};
+  using Elem = std::pair<int, std::string>;
+  std::priority_queue<Elem, std::vector<Elem>, std::greater<>> cola_de_prioridad;
+  std::string palabra = prefijo;
+  recolectar_topk(nodo, palabra, cola_de_prioridad, k);
+  // Convertir y ordenar de mayor a menor
+  std::vector<std::pair<int, std::string>> resultado;
+  resultado.reserve(cola_de_prioridad.size());
+  while (!cola_de_prioridad.empty()) {
+    resultado.push_back(cola_de_prioridad.top());
+    cola_de_prioridad.pop();
+  } // el resultado es un par<frecuencia, palabra>
+  std::reverse(resultado.begin(), resultado.end());
+  return resultado;
+}
+
+void Trie::recolectar_topk(
+    NodoTrie *nodo,
+    std::string &prefijo,
+    std::priority_queue<
+        std::pair<int, std::string>,
+        std::vector<std::pair<int, std::string>>, // este es un contenedor interno para los pares (protocolo de creación de un priority_queue)
+        std::greater<> // esto hace que automaticamente con pop() se saque al peor "greater"!!! (ie. al menos frecuente)
+    > &cola_de_prioridad, // esta cola tiene a lo mucho k elementos
+    int k) const {
+  if (!nodo)
+    return;
+  if (nodo->fin_de_palabra) {
+    if ((int)cola_de_prioridad.size() < k) // si la cola no está llena, se mete la palabra con su frecuencia
+      cola_de_prioridad.emplace(nodo->frecuencia, prefijo); // emplace es un enqueue eficiente
+    else if (nodo->frecuencia > cola_de_prioridad.top().first) { // como el peor siempre está al inicio
+      cola_de_prioridad.pop(); // si el actual es mas frecuente que el top, se saca el top
+      cola_de_prioridad.emplace(nodo->frecuencia, prefijo); // y se mete al end de la cola
+    }
+  }
+  // recorrer hijos
+  for (auto &[c, hijo] : nodo->hijos) { // las tres lineas siguientes son patrón universal de backtracking
+    prefijo.push_back(c);
+    recolectar_topk(hijo, prefijo, cola_de_prioridad, k);
+    prefijo.pop_back(); // muy eficiente, se restaura el estado
+  }
+}
--- U3/autocompletado/src/trie.hpp
+++ U3/autocompletado/src/trie.hpp
@@ -1,11 +1,11 @@
 #ifndef TRIE_HPP
 #define TRIE_HPP
 
+#include <algorithm>
+#include <queue>
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <algorithm>
-
 
 struct NodoTrie {
   std::unordered_map<char, NodoTrie *> hijos;
@@ -17,9 +17,18 @@ struct NodoTrie {
 class Trie {
 private:
   NodoTrie *raiz;
-  void destruir(NodoTrie* nodo);
+  void destruir(NodoTrie *nodo);
   // Función recursiva para recolectar palabras desde un nodo dado
   void recolectar(NodoTrie *nodo, const std::string &prefijo, std::vector<std::pair<std::string, int>> &resultados) const;
+  // el mismo de arriba pero más eficiente
+  void recolectar_topk(
+    NodoTrie *nodo,
+    std::string &prefijo,
+    std::priority_queue<
+        std::pair<int, std::string>,
+        std::vector<std::pair<int, std::string>>,
+        std::greater<>> &cola_de_prioridad,
+    int k) const;
 
 public:
   Trie();
@@ -33,7 +42,9 @@ public:
 
   // Obtener sugerencias basadas en un prefijo
   // Devuelve pares (palabra, frecuencia)
-  std::vector<std::pair<std::string, int>>obtener_sugerencias(const std::string &prefijo, int limite) const;
+  std::vector<std::pair<std::string, int>> obtener_sugerencias(const std::string &prefijo, int limite) const;
+  // el mismo de arriba pero mas eficiente
+  std::vector<std::pair<int, std::string>> obtener_topk(const std::string &prefijo, int k = 10) const;
 };
 
 #endif // TRIE_HPP
